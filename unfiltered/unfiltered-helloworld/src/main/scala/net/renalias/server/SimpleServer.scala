8887e4220c5027343f4b45ad67dc3b95b57e7759
Oscar Renalias <oscar@renalias.net>
2011-03-12 10:50:54 +0200

    Just playing with extractors

--- unfiltered/unfiltered-helloworld/src/main/scala/net/renalias/server/SimpleServer.scala
+++ unfiltered/unfiltered-helloworld/src/main/scala/net/renalias/server/SimpleServer.scala
@@ -1,11 +1,13 @@
 package net.renalias.server
 
-import unfiltered.response.{Ok, ResponseString}
+import unfiltered.request.{Accepts, GET, Path, Seg}
+import unfiltered.response.{NotFound, Ok, ResponseString}
 
 object SimpleServer {
-  def main(args: Array[String]) {
-    unfiltered.jetty.Http(8080).filter(unfiltered.filter.Planify {
-      case _ => Ok ~> ResponseString("Hello, world!")
-    }).run
-  }
+	def main(args: Array[String]) {
+		unfiltered.jetty.Http(8080).filter(unfiltered.filter.Planify {
+			case GET(Path(Seg("hello" :: message :: Nil))) => Ok ~> ResponseString(message)
+			case _ => Ok ~> ResponseString("Use /hello/<text>")
+		}).run
+	}
 }
