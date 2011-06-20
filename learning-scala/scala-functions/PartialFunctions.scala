46be25349797a32fadc02c5b5f9783fec3964156
Oscar Renalias <oscar@renalias.net>
2011-06-20 21:36:33 +0300

    Added a nicer example of partial functions

--- learning-scala/scala-functions/PartialFunctions.scala
+++ learning-scala/scala-functions/PartialFunctions.scala
@@ -1,17 +1,45 @@
 object PartialFunctions {
-	
-	object Divide extends PartialFunction[(Int, Int), Int] {
-			def apply(x:(Int, Int)) = x._1 / x._2
-			def isDefinedAt(x:(Int, Int)) = if(x._2 == 0) false; else true
-	}	
+	// this will save us some keystrokes
+	type Route = PartialFunction[HttpRequest, HttpResponse]	
+
+	// case classes provide extractors for pattern matching automatically
+	case class HttpRequest(path:String, params:Map[String,String] = Map())
+	case class HttpResponse(responseCode:Int, contentType:String, body:String)	
 	
 	def main(args:Array[String]) = {
-		println(Divide.isDefinedAt(4, 2)) // true
-		println(Divide.isDefinedAt(4, 0)) // false
+
+		val root: Route = {
+			case HttpRequest(path, _) if path == "/" => {
+				HttpResponse(200, "text/html", "Root path")
+			}
+		}
+	
+		val helloWorld: Route = {
+			// we can have multiple guards for pattern matching, always the most specific must go first
+			case HttpRequest(path, params) if path == "/hello/world" && params.size > 0 => {
+				// returns a string with the parameters into one long string
+				HttpResponse(200, "text/html", "Hello world with parameters = " + params.foldLeft("")({(x,y)=>x + " " + y._1.toString + " = " + y._2  + ", "}) )
+			}			
+			case HttpRequest(path, _) if path == "/hello/world" => {
+				HttpResponse(200, "text/html", "Hello, world")
+			}			
+		}
+	
+		// Using the the val syntax yields more concise and shorter code, but it's also possible to extend
+		// the PartialFunction type and create a fully-featured object or class
+		object NotFound extends Route {
+			def apply(r:HttpRequest) = HttpResponse(404, "text/plain", "Not found")
+			def isDefinedAt(r:HttpRequest) = true
+		}
+	
+		// Build our simple HTTP request processor based on combinable partial functions, see how some of the components
+		// of the composed function are defined as function literals while NotFound is defined as a function object
+		// Please note that the most specific matcher must always go first, and the least specific (catch all) last
+		val simpleServer = helloWorld orElse root orElse NotFound
 		
-		// example of using orElse - not that you'd ever want to do this...
-		val myOrElseFunction = (x:Int) => 0		
-		val divideAndNotFail = Divide orElse myOrElsefunction
-		println(divideAndNotFail((4,0)))
+		println(simpleServer(HttpRequest("/")))
+		println(simpleServer(HttpRequest("/hello/world")))
+		println(simpleServer(HttpRequest("/hello/world", Map("param1" -> "value1", "param2" -> "value2"))))
+		println(simpleServer(HttpRequest("/notfound")))
 	}
 }
